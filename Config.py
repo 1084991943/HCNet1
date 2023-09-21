5ccb2328f08597c79e0d37fe7d971d3f9f6ff3bd
1084991943 <1084991943@qq.com>
2023-09-21 10:25:18 +0800

    first

--- Config.py
+++ Config.py
@@ -5,8 +5,7 @@ HyperParams = {
     'alpha': 0.5,
     'beta':  0.5,
     'gamma': 1,
-    'kind': 'bird',
-    'bs': 20,
-    'epoch': 200,
-    'arch': 'resnet101'
+    'bs': 32,
+    'epoch': 150,
+    'arch': 'ConvNext'
 }
